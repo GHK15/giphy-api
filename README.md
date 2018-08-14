8af3dd7554de41d0cdb7ba6c57fff7e43b0d21b3
GHK15 <ginakim115@gmail.com>
2018-08-13 21:18:38 -0700

    hw upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # giphy-api
+**What the project does**
+This project utilizes the Giphy API and an ajax call to generate ten fruit-related gifs (and respective ratings) on the page. Users may click on the pre-populated buttons or input their favorite fruits to create new buttons that add ten more gifs to the page. Clicking on the static gif animates it; another click turns the gif static again.
+
+**Libraries / Tools**
+1. Bootstrap
+2. jQuery
+3. Giphy API
---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+var fruits = ["strawberry", "watermelon", "lemon", "pineapple"];
+
+    function displayFruitInfo() {
+
+        var fruit = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + fruit + "&api_key=Zzr283Wxs9cdxOHkBglvUGGm9M6ksBPI&limit=10";
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        var gifs = response.data;
+
+        for (var i = 0; i < gifs.length; i++) {
+        var fruitDiv = $("<div class='fruit jumbotron mx-auto'>");
+        var rtDiv = $('<div>');
+        var ratings = response.data[i].rating;
+        var pOne = $("<h2 class='text-center'>").text("Rated: " + ratings);
+        rtDiv.append(pOne);
+
+        var imgUrl = response.data[i].images.fixed_height_still.url;
+        var moveUrl = response.data[i].images.fixed_height.url;
+
+        var image = $("<img class='img-responsive mx-auto gif' style='display:block'>").attr("src", imgUrl);
+        image.attr("data-state", "");
+        image.attr("alt", "fruit gif");
+        image.attr("data-still", imgUrl);
+        image.attr("data-animate", moveUrl);
+
+        fruitDiv.append(rtDiv)
+        fruitDiv.append(image);
+
+        $("#fruits-view").prepend(fruitDiv);
+
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+                console.log("it was still");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+                console.log("it was animated")
+            }
+        });
+        
+        }
+
+        });
+
+    }
+
+
+    function renderButtons() {
+
+        $("#buttons-view").empty();
+
+        for (var i = 0; i < fruits.length; i++) {
+        var a = $("<button>");
+        a.addClass("fruit-btn btn btn-danger ml-4 mb-3");
+        a.attr("data-name", fruits[i]);
+        a.text(fruits[i]);
+        $("#buttons-view").append(a);
+        }
+    }
+
+    $("#add-fruit").on("click", function(event) {
+        event.preventDefault();
+        var fruit = $("#fruit-input").val().trim();
+        fruits.push(fruit);
+
+        renderButtons();
+    });
+
+    $(document).on("click", ".fruit-btn", displayFruitInfo);
+
+    renderButtons();
